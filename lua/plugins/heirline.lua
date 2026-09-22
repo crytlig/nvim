d63ad03cbf9a6341aaa4c37699cb97b9c006204a
Claes Rytlig <crytlig@hotmail.com>
2026-09-22 09:50:25 +0200

    add markdown and format

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -24,7 +24,7 @@ return {
     opts.statusline = { -- statusline
       hl = { fg = "fg", bg = "bg" },
       -- codeium_status,
-      status.component.mode({ mode_text = { padding = { left = 1, right = 1} } }),
+      status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }),
       status.component.git_branch(),
       status.component.file_info(),
       status.component.git_diff(),
@@ -70,14 +70,14 @@ return {
     }
 
     opts.tabline = { -- tabline
-      { -- file tree padding
+      {              -- file tree padding
         condition = function(self)
           self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
           self.winwidth = vim.api.nvim_win_get_width(self.winid)
           return self.winwidth ~= vim.o.columns -- only apply to sidebars
-            and not require("astrocore.buffer").is_valid(
-              vim.api.nvim_win_get_buf(self.winid)
-            ) -- if buffer is not in tabline
+              and not require("astrocore.buffer").is_valid(
+                vim.api.nvim_win_get_buf(self.winid)
+              ) -- if buffer is not in tabline
         end,
         provider = function(self)
           return (" "):rep(self.winwidth + 1)
@@ -85,11 +85,11 @@ return {
         hl = { bg = "tabline_bg" },
       },
       status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
-      status.component.fill({ hl = { bg = "tabline_bg" } }), -- fill the rest of the tabline with background color
-      { -- tab list
+      status.component.fill({ hl = { bg = "tabline_bg" } }),              -- fill the rest of the tabline with background color
+      {                                                                   -- tab list
         condition = function()
           return #vim.api.nvim_list_tabpages() >= 2
-        end, -- only show tabs if there are more than one
+        end,                           -- only show tabs if there are more than one
         status.heirline.make_tablist({ -- component for each tab
           provider = status.provider.tabnr(),
           hl = function(self)
